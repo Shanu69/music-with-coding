7565a9a762baa2bc376c9658fd794307e51f09d5
Shantanu <shantanu456rai@gmail.com>
2022-07-24 17:14:35 +0530

    first commit

---
+++ Ayush.js
@@ -0,0 +1,384 @@
+ 
+ var audio7 = new Audio("Audio/audio7.mp3");
+ var audio6 = new Audio("Audio/audio6.mp3");
+ var audio5 = new Audio("Audio/audio5.mp3");
+ var audio4 = new Audio("Audio/audio4.mp3");
+ var audio3 = new Audio("Audio/audio3.mp3");
+ var audio2 = new Audio("Audio/audio2.mp3");
+ var audio1 = new Audio("Audio/audio1.mp3");
+
+ window.onload=function(){
+    document.getElementById("my_audio").play();
+  }
+
+function my_magic7()
+{ if(audio7.paused)
+    {  
+        audio7.play();
+        audio6.pause();
+        audio5.pause();
+        audio4.pause();
+        audio3.pause();
+        audio2.pause();
+        audio1.pause();
+        document.getElementById("my_audio").pause();
+    }
+    else
+    {
+      audio7.pause();
+
+
+    }
+
+   
+    document.body.style.backgroundImage="url(https://cdna.artstation.com/p/assets/images/images/024/049/972/original/daniel-tolmachov-redhand02.gif?1581160925)";
+    document.getElementById("ww").style.color="#D0011B";
+    document.getElementById("w").style.color="rgb(255, 0, 0)";
+    document.getElementById("w").style.backgroundImage="url(https://cdna.artstation.com/p/assets/images/images/024/049/972/original/daniel-tolmachov-redhand02.gif?1581160925)";
+    document.getElementById("w1").style.color="rgb(255, 0, 0)";
+    document.getElementById("w1").style.backgroundImage="url(https://cdna.artstation.com/p/assets/images/images/024/049/972/original/daniel-tolmachov-redhand02.gif?1581160925)";
+    document.getElementById("w2").style.color="rgb(255, 0, 0)";
+    document.getElementById("w2").style.backgroundImage="url(https://cdna.artstation.com/p/assets/images/images/024/049/972/original/daniel-tolmachov-redhand02.gif?1581160925)";
+    document.getElementById("pain").style.backgroundImage="url(https://c.tenor.com/5nON1L6KUqcAAAAd/sharingan-naruto.gif)";
+    document.getElementById("pain").style.width="150px";
+    document.getElementById("pain").style.height="150px";
+    document.getElementById("pain").style.backgroundSize="contain";
+}
+function my_magic6()
+{ if(audio6.paused)
+    {
+        audio6.play();
+        audio7.pause();
+        audio5.pause();
+        audio4.pause();
+        audio3.pause();
+        audio2.pause();
+        audio1.pause();
+        document.getElementById("my_audio").pause();
+    }
+    else
+    {
+      audio6.pause();
+
+
+    }
+
+
+    document.body.style.backgroundImage="url(https://cdna.artstation.com/p/assets/images/images/021/649/752/original/daniel-tolmachov-explosion09.gif?1572454515)";
+    document.getElementById("ww").style.color="#FF7F00";
+    document.getElementById("w").style.color="#FF7F00";
+    document.getElementById("w").style.backgroundImage="url(https://cdna.artstation.com/p/assets/images/images/021/649/752/original/daniel-tolmachov-explosion09.gif?1572454515)";
+    document.getElementById("w1").style.color="#FF7F00";
+    document.getElementById("w1").style.backgroundImage="url(https://cdna.artstation.com/p/assets/images/images/021/649/752/original/daniel-tolmachov-explosion09.gif?1572454515)";
+    document.getElementById("w2").style.color="#FF7F00";
+    document.getElementById("w2").style.backgroundImage="url(https://cdna.artstation.com/p/assets/images/images/021/649/752/original/daniel-tolmachov-explosion09.gif?1572454515)";
+    document.getElementById("pain").style.backgroundImage="url(https://c.tenor.com/KYVGTRWokQoAAAAC/death-note-light-yagami.gif)";
+    document.getElementById("pain").style.width="150px";
+    document.getElementById("pain").style.height="150px";
+    document.getElementById("pain").style.backgroundSize="cover";
+
+
+
+
+
+}
+function my_magic5()
+{
+    if(audio5.paused)
+    {
+        audio5.play();
+        audio6.pause();
+        audio7.pause();
+        audio4.pause();
+        audio3.pause();
+        audio2.pause();
+        audio1.pause();
+        document.getElementById("my_audio").pause();
+    }
+    else
+    {
+      audio5.pause();
+
+
+    }
+
+
+
+    document.body.style.backgroundImage="url(https://cdnb.artstation.com/p/assets/images/images/024/445/479/original/daniel-tolmachov-phoenix03.gif?1582456720)";
+    document.getElementById("ww").style.color="#FFBF00";
+    document.getElementById("w").style.color="#FFBF00";
+    document.getElementById("w").style.backgroundImage="url(https://cdnb.artstation.com/p/assets/images/images/024/445/479/original/daniel-tolmachov-phoenix03.gif?1582456720)";
+    document.getElementById("w1").style.color="#FFBF00";
+    document.getElementById("w1").style.backgroundImage="url(https://cdnb.artstation.com/p/assets/images/images/024/445/479/original/daniel-tolmachov-phoenix03.gif?1582456720)";
+    document.getElementById("w2").style.color="#FFBF00";
+    document.getElementById("w2").style.backgroundImage="url(https://cdnb.artstation.com/p/assets/images/images/024/445/479/original/daniel-tolmachov-phoenix03.gif?1582456720)";
+    document.getElementById("pain").style.backgroundImage="url(https://c.tenor.com/SSmEVvAuKesAAAAC/ace-sabo.gif)";
+    document.getElementById("pain").style.width="150px";
+    document.getElementById("pain").style.height="150px";
+    document.getElementById("pain").style.backgroundSize="contain";
+  }
+function my_magic4()
+{
+
+    if(audio4.paused)
+    {
+        audio4.play();
+        audio6.pause();
+        audio5.pause();
+        audio7.pause();
+        audio3.pause();
+        audio2.pause();
+        audio1.pause();
+        document.getElementById("my_audio").pause();
+    }
+    else
+    {
+      audio4.pause();
+
+
+    }
+
+
+    document.body.style.backgroundImage="url(https://cdna.artstation.com/p/assets/images/images/023/460/038/original/daniel-tolmachov-silence01.gif?1579270773)";
+    document.getElementById("ww").style.color="#301934";
+    document.getElementById("w").style.color="rgb(160, 0, 252)";
+    document.getElementById("w").style.backgroundImage="url(https://cdna.artstation.com/p/assets/images/images/023/460/038/original/daniel-tolmachov-silence01.gif?1579270773)";
+    document.getElementById("w1").style.color="#301934";
+    document.getElementById("w1").style.backgroundImage="url(https://cdna.artstation.com/p/assets/images/images/023/460/038/original/daniel-tolmachov-silence01.gif?1579270773)";
+    document.getElementById("w2").style.color="#301934";
+    document.getElementById("w2").style.backgroundImage="url(https://c.tenor.com/0NZ4t7ti0uwAAAAC/blue-fire-pyrotechnics.gif)";
+    document.getElementById("pain").style.backgroundImage="url(https://i.pinimg.com/originals/f2/68/80/f268801c58b0456db40ee51b237d407a.gif)";
+    document.getElementById("pain").style.width="150px";
+    document.getElementById("pain").style.height="150px";
+    document.getElementById("pain").style.backgroundSize="cover";
+  
+
+
+}
+function my_magic3()
+{
+    if(audio3.paused)
+    {
+        audio3.play();
+        audio6.pause();
+        audio5.pause();
+        audio4.pause();
+        audio7.pause();
+        audio2.pause();
+        audio1.pause();
+        document.getElementById("my_audio").pause();
+    }
+    else
+    {
+      audio3.pause();
+
+
+    }
+    
+     
+    document.body.style.backgroundImage="url(https://c.tenor.com/0NZ4t7ti0uwAAAAC/blue-fire-pyrotechnics.gif)";
+    document.getElementById("ww").style.color="#3366CC";
+    document.getElementById("w").style.color="rgb(6, 41, 179)";
+    document.getElementById("w").style.backgroundImage="url(https://c.tenor.com/0NZ4t7ti0uwAAAAC/blue-fire-pyrotechnics.gif)";
+    document.getElementById("w1").style.color="rgb(6, 41, 179)";
+    document.getElementById("w1").style.backgroundImage="url(https://c.tenor.com/0NZ4t7ti0uwAAAAC/blue-fire-pyrotechnics.gif)";
+    document.getElementById("w2").style.color="rgb(6, 41, 179)";
+    document.getElementById("w2").style.backgroundImage="url(https://cdna.artstation.com/p/assets/images/images/023/460/038/original/daniel-tolmachov-silence01.gif?1579270773)";
+    document.getElementById("pain").style.backgroundImage="url(gif/gif1.gif),url(https://cutewallpaper.org/21/blue-background-gif/GIF-blue-animated-GIF-on-GIFER-by-Grotaur.gif)";
+    document.getElementById("pain").style.width="150px";
+    document.getElementById("pain").style.height="150px";
+    document.getElementById("pain").style.backgroundSize="contain , cover";
+    
+  }
+
+
+function my_magic2()
+{
+
+    if(audio2.paused)
+    {
+        audio2.play();
+        audio6.pause();
+        audio5.pause();
+        audio4.pause();
+        audio3.pause();
+        audio7.pause();
+        audio1.pause();
+        document.getElementById("my_audio").pause();
+    }
+    else
+    {
+      audio2.pause();
+
+
+    }
+
+
+    document.body.style.backgroundImage="url(https://cdnb.artstation.com/p/assets/images/images/023/690/563/original/daniel-tolmachov-clover3.gif?1580051200)";
+    document.getElementById("ww").style.color="#1E7D22";
+    document.getElementById("w").style.color="rgb(0, 255, 0)";
+    document.getElementById("w").style.backgroundImage="url(https://cdnb.artstation.com/p/assets/images/images/023/690/563/original/daniel-tolmachov-clover3.gif?1580051200)";
+    document.getElementById("w1").style.color="rgb(0, 255, 0)";
+    document.getElementById("w1").style.backgroundImage="url(https://cdnb.artstation.com/p/assets/images/images/023/690/563/original/daniel-tolmachov-clover3.gif?1580051200)";
+    document.getElementById("w2").style.color="rgb(0, 255, 0)";
+    document.getElementById("w2").style.backgroundImage="url(https://cdnb.artstation.com/p/assets/images/images/023/690/563/original/daniel-tolmachov-clover3.gif?1580051200)";
+    document.getElementById("pain").style.backgroundImage="url(https://i.pinimg.com/originals/6b/0f/79/6b0f79fd86645bec091d4b278ca99237.gif)";
+    document.getElementById("pain").style.width="150px";
+    document.getElementById("pain").style.height="150px";
+    document.getElementById("pain").style.backgroundSize="cover";
+    
+}
+function my_magic1()
+{
+    if(audio1.paused)
+    {
+        audio1.play();
+        audio6.pause();
+        audio5.pause();
+        audio4.pause();
+        audio3.pause();
+        audio2.pause();
+        audio7.pause();
+        document.getElementById("my_audio").pause();
+    }
+    else
+    {
+      audio1.pause();
+
+
+    }
+
+
+    document.body.style.backgroundImage="url(https://c.tenor.com/18SBZln9s3AAAAAC/black-space-outer-space.gif)";
+    document.getElementById("ww").style.color="gray";
+    document.getElementById("w").style.color="gray";
+    document.getElementById("w").style.backgroundImage="url(https://c.tenor.com/18SBZln9s3AAAAAC/black-space-outer-space.gif)";
+    document.getElementById("w1").style.color="gray";
+    document.getElementById("w1").style.backgroundImage="url(https://c.tenor.com/18SBZln9s3AAAAAC/black-space-outer-space.gif)";
+    document.getElementById("w2").style.color="gray";
+    document.getElementById("w2").style.backgroundImage="url(https://c.tenor.com/18SBZln9s3AAAAAC/black-space-outer-space.gif)";
+document.getElementById("pain").style.backgroundImage="url(https://c.tenor.com/auVQ5lpK7ZYAAAAM/doraemon-working-out.gif)";
+    document.getElementById("pain").style.width="150px";
+    document.getElementById("pain").style.height="150px";
+    document.getElementById("pain").style.backgroundSize="contain";
+
+}
+
+
+
+var card= document.getElementById("card");
+var sound1 = new Audio("sound/sound1.mp3");
+
+
+
+
+function openregister()
+{
+
+   sound1.play();
+   sound1.playbackRate=1.5;
+  card.style.transform = "rotateY(-180deg)";
+  document.getElementById("cb").style.backgroundImage = "linear-gradient(rgb(95, 95, 95),rgb(85, 85, 85)),url(https://cdnb.artstation.com/p/assets/images/images/043/516/811/original/pochekunina-yana-123.gif?1637510631)";
+  document.getElementById("op").style.backgroundImage = "url(https://cdna.artstation.com/p/assets/images/images/041/221/366/original/pochekunina-yana-.gif?1631102177)";
+}
+function openlogin()
+{
+	sound1.play();
+	sound1.playbackRate=1.5;
+  card.style.transform = "rotateY(0deg)";
+   document.getElementById("cv").style.backgroundImage = "linear-gradient(rgb(95, 95, 95),rgb(85, 85, 85)),url(https://cdnb.artstation.com/p/assets/images/images/034/925/029/original/dmitry-sarkisov-.gif?1613606118)";
+   document.getElementById("op").style.backgroundImage = "url(https://cdnb.artstation.com/p/assets/images/images/034/925/029/original/dmitry-sarkisov-.gif?1613606118)";
+}
+
+
+
+function store(){
+
+    sound1.play();
+   sound1.playbackRate=1.5;
+    var name = document.getElementById('name');
+    var pw = document.getElementById('pw');
+    var email = document.getElementById('email');
+    var lowerCaseLetters = /[a-z]/g;
+    var upperCaseLetters = /[A-Z]/g;
+    var numbers = /[0-9]/g;
+
+    if(name.value.length == 0 || email.value.length==0){
+        alert('Please fill in email');
+
+    }else if(pw.value.length == 0){
+        alert('Please fill in password');
+
+    }else if((name.value.length == 0 && pw.value.length == 0)||(email.value.length == 0 && pw.value.length == 0)){
+        alert('Please fill in email and password');
+
+    }else if(pw.value.length > 8){
+        alert('Max of 8');
+
+    }else if(!pw.value.match(numbers)){
+        alert('please add 1 number');
+
+    }else if(!pw.value.match(upperCaseLetters)){
+        alert('please add 1 uppercase letter');
+
+    }else if(!pw.value.match(lowerCaseLetters)){
+        alert('please add 1 lovercase letter');
+
+    }else{
+        localStorage.setItem('name', name.value);
+        localStorage.setItem('email',email.value)
+        localStorage.setItem('pw', pw.value);
+        alert('Your account has been created');
+    }
+}
+
+//checking
+function check(){
+    sound1.play();
+    sound1.playbackRate=1.5;
+
+    var storedName = localStorage.getItem('name');
+    var storedPw = localStorage.getItem('pw');
+    var storedemail = localStorage.getItem('email');
+
+    var userName = document.getElementById('userName');
+    var userPw = document.getElementById('userPw');
+    var userRemember = document.getElementById("rememberMe");
+
+    if((userName.value == storedName && userPw.value == storedPw))
+    {
+        alert('You are logged in.');
+        
+
+		window.location.href = 'Ayush.html';
+
+    }
+    else if((userName.value == storedemail && userPw.value == storedPw))
+    {
+        alert('You are logged in.');
+        
+
+		window.open('Ayush.html');
+    
+    }
+    else{
+        alert('Error on login');
+        
+    }
+}
+
+function master()
+{
+
+    audio7.pause();
+    audio6.pause();
+    audio5.pause();
+    audio4.pause();
+    audio3.pause();
+    audio2.pause();
+    audio1.pause();
+    document.getElementById("my_audio").pause();
+    sound1.play();
+    window.open('profile.html');
+
+}
